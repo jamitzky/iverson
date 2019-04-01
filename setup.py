4e5b4de089f1b1e2198741876171b1aa2a0270cc
Ferdinand Jamitzky <f.jamitzky@gmail.com>
2019-04-01 18:33:59 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='iverson',
+      version='0.1',
+      description='mimick J syntax',
+      author='Ferdinand Jamitzky',
+      author_email='f.jamitzky@gmail.com',
+      url='https://github.com/jamitzky/iverson',
+      packages=[],
+     )
